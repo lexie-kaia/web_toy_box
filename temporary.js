4bd9c5b094160aa652447db86204f14a30645ae6
lexie-kaia <kaia.gayeon.kim@gmail.com>
2021-04-04 14:39:40 +0900

    reset : normalize + reset -> custom reset.css

--- temporary.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('.js-nav');
-const btnMore = document.querySelector('.js-btn-more');
-
-const toggleMenu = () => {
-  nav.classList.toggle('js-display-block');
-};
-
-btnMore.addEventListener('click', toggleMenu);
